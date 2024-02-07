e03b35123ca618b327c7a4fd341dee0f3c3b0d82
Tulio Marcos Dias da Silva <tuliodias@yahoo.com.br>
2024-02-07 11:56:16 -0200

    commit 2 info F

--- src/sintaxe/Teste.java
+++ src/sintaxe/Teste.java
@@ -4,11 +4,35 @@
  * and open the template in the editor.
  */
 package sintaxe;
+import java.util.Scanner;
+
 
 /**
  *
  * @author tulio
  */
 public class Teste {
+    
+    public static void main(String[] args) {
+       
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Digite seu nome:");
+        String nome = entrada.nextLine();
+        System.out.println("Digite sua idade:");
+        int idade = entrada.nextInt();
+        System.out.println("Digite seu salário:");
+        double salario = entrada.nextDouble();
+         
+        
+        System.out.println("Nome:"+nome);
+        System.out.println("Idade:"+idade);
+        System.out.println("Salário: "+salario);
+        
+     System.out.println("Nome: "+nome+" Idade: "+idade+" Salário: "+salario);
+        
+    }
+ 
+    
+      
     
 }
